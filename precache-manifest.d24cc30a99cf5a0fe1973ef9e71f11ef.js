582ec199da1162d56da7b9cb36323a66616117b7
Artjoms <artjoms.t.wiredelta@gmail.com>
2020-12-10 15:40:39 +0100

    Updates

---
+++ precache-manifest.d24cc30a99cf5a0fe1973ef9e71f11ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec47391df673bc96b7a1fddf81ababa",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "81db705d2346f1722843",
+    "url": "/react-weather/static/css/2.6a770793.chunk.css"
+  },
+  {
+    "revision": "127cc7d339621ddd6440",
+    "url": "/react-weather/static/css/main.e04739c1.chunk.css"
+  },
+  {
+    "revision": "81db705d2346f1722843",
+    "url": "/react-weather/static/js/2.4a184d67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.4a184d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127cc7d339621ddd6440",
+    "url": "/react-weather/static/js/main.bb047db4.chunk.js"
+  },
+  {
+    "revision": "268240879776a3012783",
+    "url": "/react-weather/static/js/runtime-main.8bbffba3.js"
+  }
+]);
